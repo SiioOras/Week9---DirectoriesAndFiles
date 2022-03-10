01f6eb44c4c244cf9e8c3210339dc70db49ce672
Siio Oras <soras@tktk.com>
2022-03-10 16:01:23 +0200

    add new projects

---
+++ ShoppingList/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ShoppingList
+{
+    class Program
+    {
+        private static IEnumerable<string> myShoppingLists;
+        private static object fileLocation;
+
+        static void Main(string[] args)
+        {
+            string rootDirectory = @"C:\Users\Laptop\samples";
+            Console.WriteLine("Enter directory name:");
+            string newDirectory = Console.ReadLine();
+            Console.WriteLine("Enter file name:");
+            string fileName = Console.ReadLine();
+
+            if (Directory.Exists($"{rootDirectory}\\{newDirectory}") && File.Exists($"{rootDirectory}\\{newDirectory}\\{fileName}"))
+            {
+                Console.WriteLine("Directory and File already exists.");
+            }
+            else
+            {
+                Directory.CreateDirectory($"{rootDirectory}\\{newDirectory}");
+                File.Create($"{rootDirectory}\\{newDirectory}\\{fileName}");
+            }
+        }
+    }
+}
